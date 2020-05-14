f17f7a263a3e6c1bfc353d8994c789d6f15e5e32
Thulasi Ram <thulasi503@gmail.com>
2020-05-14 19:50:37 +0530

    Reduce no of js files

---
+++ assets/ahiravan.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+
+  const TABS = [...document.querySelectorAll('#tabs li')];
+  const CONTENT = [...document.querySelectorAll('#tab-content div')];
+  const ACTIVE_CLASS = 'is-active';
+
+  function initTabs() {
+    TABS.forEach((tab) => {
+      tab.addEventListener('click', (e) => {
+        let selected = tab.getAttribute('data-tab');
+        updateActiveTab(tab);
+        updateActiveContent(selected);
+      })
+    })
+  }
+
+  function updateActiveTab(selected) {
+    TABS.forEach((tab) => {
+      if (tab && tab.classList.contains(ACTIVE_CLASS)) {
+        tab.classList.remove(ACTIVE_CLASS);
+      }
+    });
+    selected.classList.add(ACTIVE_CLASS);
+  }
+
+  function updateActiveContent(selected) {
+    CONTENT.forEach((item) => {
+      if (item && item.classList.contains(ACTIVE_CLASS)) {
+        item.classList.remove(ACTIVE_CLASS);
+      }
+      let data = item.getAttribute('data-content');
+      if (data === selected) {
+        item.classList.add(ACTIVE_CLASS);
+      }
+    });
+  }
+  initTabs();
+
+
+  if (document.getElementById("typed")) {
+    var typed1 = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+      showCursor: false,
+      typeSpeed: 10,
+      backSpeed: 10,
+      startDelay: 50,
+    });
+  };
+
+
+
+});
